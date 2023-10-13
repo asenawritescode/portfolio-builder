bebbceba39b8dc659e1951a573ae3b41387851eb
ezratechdev <254gtgt@gmail.com>
2023-10-13 09:54:29 +0300

    Data upload arrays

---
+++ routes/account.js
@@ -0,0 +1,41 @@
+const 
+express = require("express"),
+account_router = express.Router()
+;
+
+account_router
+// middlewares
+
+
+// endpoints
+.post('/details' , async function(req , res){
+    /**
+     * DETAILS TO FETCH
+     * ARRAYS: 
+     *          Education 
+     *          Experience
+     *          DashBoard Menu Links
+     *          Projects : Enable sorting , running projects , feature project 
+     *          Testimonials 
+     *          Social media links
+     *          Skills
+     * Texts:
+     *          Name
+     *          Role
+     *          Bio
+     *          Number
+     *          DOB
+     *          Email
+     *          Location
+     * Files:
+     *          Photos
+     *          Videos
+     * Customizations:
+     *          Color
+     * 
+     * 
+     */
+})
+
+// 
+module.exports = account_router
