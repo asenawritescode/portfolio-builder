f2f3bdb98ed5ad96fec467603a88cb799cdf4789
asenawritescode <samasena2001@gmail.com>
2023-12-16 12:27:02 +0300

    Removed redundant files

--- index.js
+++ index.js
@@ -1,144 +1,143 @@
-const express = require('express');
-const cors = require('cors')
-const helmet = require('helmet');
-const xss = require('xss-clean');
-const path = require('path')
+const
+    express = require("express"),
+    app = express(),
+    path = require("path"),
+    ejs = require("ejs"),
+    port = process.env.PORT || 5700
+    ;
 
-const PORT = process.env.PORT || 3000
 
-const app = express();
+// connect to the database
+require("./functions/connectDb")
 
-// Set EJS as the view engine
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs');
+app
+    // configs
+    .use(express.json())
+    .use(express.urlencoded())
+    .use(require("helmet")())
+    // make files accesible
+    .use(express.static(path.join(`${__dirname}/views/`)))
+    .use(express.static(path.join(__dirname, 'node_modules')))
+    .use(express.static(path.join(__dirname, 'public')))
 
-app.use(cors());
-app.use(helmet());
-app.use(xss());
-app.use(express.json());
-app.use(express.urlencoded());
+    // view engine
+    .set('view engine', 'ejs')
+    .set('views', `${__dirname}/views`)
 
-// Simulated JSON-based database
-let database = {};
 
-const generateId = () => {
-  const randomId = Math.random().toString(36);
-  return randomId.slice(2, 11);
-};
+    // mids
+    .get('/', function (req, res) {
+        // restrict access points , generate and ssl and pin
+        return res.render('index');
+    })
 
-app.use(express.static(path.join(__dirname, 'public')));
+    //  user-profile
+    .get('/user/:userId', function (req, res) {
+        const userId = req.params.userId;
+        // Use the userId to load the specific user page
+        if (!userId) {
+            return res.status(404).send('User not found');
+        }
 
-// Health check endpoint
-app.get('/healthcheck', (_, res) => {
-  res.send('Server is up and running');
-});
+        // get data from db 
+        let userData
+        // const userData = await User.findById(userId);
+        if (!userData) {
+            return res.status(404).send('User not found');
+        }
 
-app.get('/template', (req, res)=>{
-  // TODO : render the template based on file name 
-    res.render('index');
+        return res.render('user', { data: userData });
+    })
+
+
+    // form
+    .get('/form', function (req, res) {
+        return res.render('form', { dat: 'Dat' })
+    })
+
+    // get qr code
+    .get('/qr/:id', function (req, res) {
+        return res.send(req.params.id)
+    })
+
+    // .get('/hc' , function(_ , res){
+    //     return res.send(`Hi`)
+    // })
+
+    .get('/:fileName.html', function (req, res, next) {
+        /**
+         * THE /:id.html is going to capture all the redirects from the ejs 
+         * I have no time to create a router
+         * SO CAPTCHA ALL THE IDS IE : n.ejs and render n if it's responding ejs file is present
+         * if file if not present , throw a 404 error by invoking next
+         */
+        const fileName = String(req.params.fileName).trim();
+        console.log(fileName);
+        // pass cookie from here the check cookie from reading pages
+        // check from 
+        const cookie = 1;
+        try {
+            res
+                // This render function should be the "last thing" sent back to the client
+                .render(`${fileName}`, { cookie })
+            /**
+             * THIS BUG WAS SELF INDUCED , 
+             * IF THE EJS FILE IS NOT FOUND THE TRIGGER BUG AND SEND 404 ERROR BELOW
+             */
+            // .cs('cooks' , cookie , { signed: true });
+        } catch (er) {
+            res.status(404).send(
+                `
+            <h2>${fileName}.html does not exits on this server<h2/>
+            <br />
+            <a href="/">Go home</a>
+            `
+            );
+            console.log()
+            console.log(`Er : `, er);
+            console.log()
+            console.log(`lorem ipsum`)
+        }
+    })
+
+    .get('/:fileName/:id.html', function (req, res, next) {
+        // send back file with id , get data and loop and pass it through to the ejs file and loop , data to be fetched from db
+        /**
+         * THE /:id.html is going to capture all the redirects from the ejs 
+         * I have no time to create a router
+         * SO CAPTCHA ALL THE IDS IE : n.ejs and render n if it's responding ejs file is present
+         * if file if not present , throw a 404 error by invoking next
+         */
+        return res.send(`${String(req.params.fileName).trim()} : id ${String(req.params.id).trim()}`)
+        const fileName = String(req.params.fileName).trim();
+        console.log(fileName);
+        // pass cookie from here the check cookie from reading pages
+        // check from 
+        const cookie = 1;
+        try {
+            res
+                .render(`${fileName}`, { cookie })
+                /**
+                 * THIS BUG WAS SELF INDUCED , 
+                 */
+                .cs('cooks', cookie, { signed: true })
+                ;
+        } catch (er) {
+            res.status(500).send('Internal Server Error');
+            console.log()
+            console.log(`Er : `, er);
+            console.log()
+            console.log(`lorem ipsum`)
+        }
+    })
+    // 404 error
+    .use(function (req, res) {
+        return res.status(404)
+            .send(`<h1>404 error</h1>`)
+    })
+
+
+// dbs
+require("http").createServer(app).listen(port, undefined, undefined, function (er) {
+    console.log(`Server ${er ? "crashing" : "running"} on port ${port}`)
 })
-
-// GET all users
-app.get('/users/all', (_, res) => {
-  res.send(database);
-});
-
-// GET user by ID
-app.get('/users/:id', (req, res) => {
-  const userId = req.params.id;
-  const user = database[userId];
-  if (user) {
-    res.send(user);
-  } else {
-    res.status(404).send('User not found');
-  }
-});
-
-app.post('profile', (req, res) => {
-    res.render('profile');
-});
-
-// POST request
-app.post('/users/new', (req, res) => {
-  const id = generateId();
-  const { name, email, age } = req.body;
-  database[id] = {
-    name: name,
-    email: email,
-    age: age
-  };
-  res.send({ id });
-});
-
-// PUT request
-/**
- * This updates the entire resource with the new data, all the data has to be present
- */
-app.put('/users/:id', (req, res) => {
-  const { name, email, age } = req?.body;
-  // check if they all exist
-  if (!name || !email || !age) {
-    res.status(400).send('Missing data');
-    return
-  }
-  
-  const id = req.params.id;
-  
-  database[id] = {
-    name: name,
-    email: email,
-    age: age
-  };
-  res.send("User updated");
-});
-
-// DELETE request by ID
-app.delete('/users/:id', (req, res) => {
-  const userId = req.params.id;
-  const user = database[userId];
-  if (user) {
-    delete database[userId];
-    res.send('User deleted !');
-  } else {
-    res.status(404).send('User not found');
-  }
-});
-
-// DELETE request
-app.delete('/', (_, res) => {
-  database = {};
-  res.send('Database cleared');
-});
-
-// PATCH request
-/**
- * This updates a particular resource, with the specified data
- */
-app.patch('/users/:id', (req, res) => {
-  const id = req.params.id;
-  const { name, age, email } = req?.body;
-  if (id in database) {
-    if (name) {
-      database[id].name = name;
-    }
-    if (email) {
-      database[id].email = email;
-    }
-    if (age) {
-      database[id].age = age;
-    } 
-    res.send('Item updated');
-  } else {
-    res.send('ID does not exist in the database');
-  }
-});
-
-// Fallback route
-app.use((req, res) => {
-  // res.status(404).send(`${req.protocol}://${req.get('host')}${req.originalUrl}`);
-  res.status(404).send(`${req.method} ${req.originalUrl} not found`);
-});
-
-app.listen(PORT, () => {
-  console.log('Server is running on port 3000');
-});
